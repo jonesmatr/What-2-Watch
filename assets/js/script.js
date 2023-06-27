1d2baa5bfc917801f365a104fab12a2a40e204a1
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-06-27 09:34:22 -0400

    Added JavaScript for the dropdown menu and search button but the API doesn't seem to be working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,98 @@
 //Create variables and assign them to the elements in the HTML
 //API Key
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '014185e72cmsh93a40585366113fp1ab874jsn76d5fb150940',
-		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-	}
-};
+// const settings = {
+// 	async: true,
+// 	crossDomain: true,
+// 	url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk',
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '014185e72cmsh93a40585366113fp1ab874jsn76d5fb150940',
+// 		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+// 	}
+// };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTY0MjRhNzZiOGRjYmEwZGU3MGI4NGZkMTJhYmRlMyIsInN1YiI6IjY0OWEyMGRlMjk3NWNhMDE0NGNjOTUwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S3lnnuQ7KF-BGan5iMJitixVDKxQUvdkl26pyTxsgo0'
-    }
-  };
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTY0MjRhNzZiOGRjYmEwZGU3MGI4NGZkMTJhYmRlMyIsInN1YiI6IjY0OWEyMGRlMjk3NWNhMDE0NGNjOTUwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S3lnnuQ7KF-BGan5iMJitixVDKxQUvdkl26pyTxsgo0'
+//     }
+//   };
   
-  fetch('https://api.themoviedb.org/3/authentication', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+//   fetch('https://api.themoviedb.org/3/authentication', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+// Select the form
+const form = document.querySelector('form');
+
+// Select the carousel
+const carousel = document.querySelector('.carousel');
+
+// Add a submit event listener to the form
+form.addEventListener('submit', event => {
+    // Get the genre
+    const genre = event.target.querySelector('select').value;
+
+    if (!genre) {
+        return;
+    }
+
+    // Prevent the form from being submitted
+    event.preventDefault();
+
+    // Call the TMDB API
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${'014185e72cmsh93a40585366113fp1ab874jsn76d5fb150940'}&with_genres=${genre}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          response.json();
+        })
+        
+        .then(data => {
+            // Clear the carousel
+            carousel.innerHTML = '';
+
+            // Loop through the movies
+            for (let movie of data.results) {
+                // Create a carousel item
+                const item = document.createElement('div');
+                item.className = 'carousel-item';
+                item.textContent = movie.title;
+
+                // Append the item to the carousel
+                carousel.appendChild(item);
+
+                // Call the Utelly API
+                (function(item) {
+                fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movie.title}&country=uk`, {
+                    headers: {
+                        'X-RapidAPI-Key': '014185e72cmsh93a40585366113fp1ab874jsn76d5fb150940',
+                        'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+                    }
+                  })
+                .then(response => {
+                  if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                  }
+                  response.json();
+                })
+
+                .then(data => {
+                    // Add the availability information to the item
+                    if(data.collection && data.collection.locations) {
+                    item.textContent += ' Available on: ' + data.collection.locations.map(location => location.name).join(', ');
+                    }
+                })
+                .catch(error => console.error('There has been a problem with your fetch operation:',error));
+            })(item);
+            }
+          })
+          .catch(error => console.error('There has been a problem with your fetch operation:',error));
+    });
