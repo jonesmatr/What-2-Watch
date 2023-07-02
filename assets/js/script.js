770da5c8d91d00fad205460be1ef6f6240dcef0e
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-07-02 09:09:39 -0400

    Added JavaScript for the search field. The user can now search for a movie on the search results page and they will be given results based on their search term.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const form = document.querySelector('form');
 const genreSelect = document.querySelector('#genre-select');
 const genreCards = document.querySelector('.card');
 
+// Select the search input and button
+const searchInput = document.querySelector('.input');
+const searchButton = document.querySelector('.button');
 
 // Fetch genres from TMDB API
 fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${'a16424a76b8dcba0de70b84fd12abde3'}&language=en-US`)
@@ -36,13 +39,97 @@ fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${'a16424a76b8dcba0
         });
     })
     .catch(error => console.error('There has been a problem with your fetch operation:', error));
+
 // Select the carousel
 const carousel = document.querySelector('.carousel');
 
-// Add a submit event listener to the form
-// form.addEventListener('submit', event => {
-    // Get the genre
-    // const genre = event.target.querySelector('select').value;
+// Add a click event listener to the search button
+searchButton.addEventListener('click', event => {
+  // Prevent the form from being submitted
+  event.preventDefault();
+
+  // Get the search query
+  const query = searchInput.value;
+
+  if (!query) {
+      return;
+  }
+
+  // Call the TMDB API
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${'a16424a76b8dcba0de70b84fd12abde3'}&query=${query}`)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error("Network response was not ok");
+          }
+          return response.json();
+      })
+      .then(data => {
+          // Clear the carousel
+          carousel.innerHTML = '';
+
+          // Loop through the movies
+          for (let movie of data.results) {
+              // Create a column for each card
+              const column = document.createElement('div');
+              column.className = 'column card movie-card is-4 m-1 level-item';
+
+              // Create a carousel item
+              const item = document.createElement('div');
+              item.className = 'level-item carousel-item is-flex-wrap-wrap';
+              item.innerHTML = `
+                <div class="card-image">
+                  <figure class="image is-4by4">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">${movie.title}</p>
+                    </div>
+                  </div>
+                  <div class="content">
+                    ${movie.overview}
+                  </div>
+                  <div class="availability"> <!-- This is the new div -->
+                    Availability: Loading...
+                  </div>
+                  </div>
+                </div>`;
+
+              // Append the item to the column
+              column.appendChild(item);
+              
+              // Append the column to the carousel
+              carousel.appendChild(column);
+
+              // Call the Utelly API
+              fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movie.title}&country=us`, {
+                  headers: {
+                      'X-RapidAPI-Key': '014185e72cmsh93a40585366113fp1ab874jsn76d5fb150940',
+                      'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+                  }
+                })
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error("Network response was not ok");
+                }
+                return response.json();
+              })
+              .then(data => {
+                  // Add the availability information to the item
+                  const availabilityDiv = item.querySelector('.availability');
+                if(data.results[0] && data.results[0].locations) {
+                  availabilityDiv.textContent = 'Availability: ' + data.results[0].locations.map(location => location.display_name).join(', ');
+                } else {
+                  availabilityDiv.textContent = 'Availability: Not available';
+                }
+              })
+              .catch(error => console.error('There has been a problem with your fetch operation:',error));
+          }
+      })
+      .catch(error => console.error('There has been a problem with your fetch operation:',error));
+});
 
 // Add an event listener to the genre select
     genreSelect.addEventListener('change', event => {
