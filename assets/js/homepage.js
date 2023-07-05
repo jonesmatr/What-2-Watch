aa0fd85813fedffc5a0d74e1e47e845c6333cc7b
jonesmatr <jonesmatr@gmail.com>
2023-07-05 16:34:30 -0400

    Formatted the JavaScript for the homepage.js file.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,16 +1,16 @@
-const homeInput = document.querySelector('.home-input');
-const homeSearch = document.querySelector('.home-search');
+const homeInput = document.querySelector(".home-input");
+const homeSearch = document.querySelector(".home-search");
 
-homeSearch.addEventListener('click', event => {
-    // Prevent the form from being submitted
-    event.preventDefault();
+homeSearch.addEventListener("click", (event) => {
+  // Prevent the form from being submitted
+  event.preventDefault();
 
-    // Get the search query
-    const query = homeInput.value;
+  // Get the search query
+  const query = homeInput.value;
 
-    // Store the search query in the local storage
-    localStorage.setItem('query', query);
+  // Store the search query in the local storage
+  localStorage.setItem("query", query);
 
-    // Redirect to the search-results.html page
-    window.location.href = 'search-results.html';
+  // Redirect to the search-results.html page
+  window.location.href = "search-results.html";
 });
